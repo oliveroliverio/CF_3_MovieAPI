624f08521020ecec59a265cee3b44122716c0d44
oliveroliverio <oliveroliverio@gmail.com>
2024-09-04 15:39:03 -0700

    using express to send to different pages depending on request

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const http = require('http'),
-	url = require('url')
+const express = require('express')
+const app = express()
 
-http
-	.createServer((request, response) => {
-		let requestURL = url.parse(request.url, true)
-		if (requestURL.pathname == '/documentation.html') {
-			response.writeHead(200, { 'Content-Type': 'text/plain' })
-			response.end('Documentation on the bookclub API.\n')
-		} else {
-			response.writeHead(200, { 'Content-Type': 'text/plain' })
-			response.end('Welcome to my book club!\n')
-		}
-	})
-	.listen(8080)
+let topBooks = [
+	{
+		title: "Harry Potter and the Sorcerer's Stone",
+		author: 'J.K. Rowling',
+	},
+	{
+		title: 'Lord of the Rings',
+		author: 'J.R.R. Tolkien',
+	},
+	{
+		title: 'Twilight',
+		author: 'Stephanie Meyer',
+	},
+]
 
-console.log('My first Node test server is running on Port 8080.')
+// GET requests
+app.get('/', (req, res) => {
+	res.send('Welcome to my book club!')
+})
+
+app.get('/documentation', (req, res) => {
+	res.sendFile('/documentation.html', { root: __dirname })
+})
+
+app.get('/books', (req, res) => {
+	res.json(topBooks)
+})
+
+// listen for requests
+app.listen(8080, () => {
+	console.log('Your app is listening on port 8080.')
+})
