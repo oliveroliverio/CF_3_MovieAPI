441ab27edfebedd4b07fb4792c7da4a144fed4bd
oliveroliverio <oliveroliverio@gmail.com>
2024-09-03 18:12:30 -0700

    ..

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
-//  import the http module and listens for requests on port 8080.
-const http = require('http')
-const port = 8080
-//import url module
-const url = require('url')
+// load node modules
+let http = require('http'),
+	fs = require('fs'),
+	url = require('url')
+// path = require('path')
+
+// create server
+http
+	.createServer((req, res) => {
+		// parse request url to see if it contains "documentation"
+		let path = new URL(req.url, `http://${req.headers.host}`).pathname
+		// if the request url contains "documentation"
+		if (path === '/documentation') {
+			// read the documentation file
+			fs.readFile('documentation.html', (err, data) => {
+				// if there is an error, return a 500 error
+				if (err) {
+					res.writeHead(500, { 'Content-Type': 'text/plain' })
+					res.end('Internal Server Error')
+				} else {
+					// return the documentation file
+					res.writeHead(200, { 'Content-Type': 'text/html' })
+					res.end(data)
+				}
+			})
+		} else {
+			// read the index.html file
+			fs.readFile('index.html', (err, data) => {
+				// if there is an error, return a 500 error
+				if (err) {
+					res.writeHead(500, { 'Content-Type': 'text/plain' })
+					res.end('Internal Server Error')
+				} else {
+					// return the index.html file
+					res.writeHead(200, { 'Content-Type': 'text/html' })
+					res.end(data)
+				}
+			})
+		}
+	})
+	// listen on port 8080
+	.listen(8080, () => {
+		console.log('Server is listening on port 8080')
+	})
