35e4d7dfe41c98fea53bd1d9b04776f2d76e127e
oliveroliverio <oliveroliverio@gmail.com>
2024-09-11 17:31:40 -0700

    added error handling

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ app.get('/movies', (req, res) => {
 	res.json(topMovies)
 })
 
+// Error-handling middleware
+app.use((err, req, res, next) => {
+	console.error(err.stack)
+	res.status(500).send('Something broke!')
+})
+
 // listen for requests
 app.listen(8080, () => {
 	console.log('Your app is listening on port 8080.')
